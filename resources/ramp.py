f382133d400a7043f8485ea51b3acd72912fa131
Ignacio Dassori <ignacio.dassori@ug.uchile.cl>
2023-08-10 18:38:30 -0400

    Minor changes

--- resources/ramp.py
+++ resources/ramp.py
@@ -8,24 +8,26 @@ class Ramp:
         self._fname = os.path.join(os.path.dirname(__file__), 'ramp.urdf')
         self._client = client
         self.path_leght = 3
-        self._ycord = 1.0
+        # Lenght of segments
+        self.L = 4
+        self._ycord = self.L-1
         self._height = 0.0
         self._pitch = 0.0
         self._currentType = 0 #0: no slope, 1: with slope
 
         #Parameters
-        self._maxPitch = 30*math.pi/180
+        self._maxPitch = 25*math.pi/180
         self._minPitch = 15*math.pi/180
         self.generate()
 
     def generate(self):
         # load starting base
-        p.loadURDF("resources/base.urdf", [0, 0, 0])
+        p.loadURDF("resources/base.urdf", [0, self.L/2-1, 0])
         # load ramps
         for _ in range(self.path_leght):
             self.init_ramp()
         # load ending base
-        p.loadURDF("resources/base.urdf", [0, self._ycord+1.0, self._height])
+        p.loadURDF("resources/base.urdf", [0, self._ycord+self.L/2, self._height])
 
     def init_ramp(self):
         self.random_pitch()
@@ -46,9 +48,8 @@ class Ramp:
             self._currentType = 1
 
     def getBasePosition(self):
-        L = 2
-        ycord = L*math.cos(self._pitch)/2
-        height = L*math.sin(self._pitch)/2
+        ycord = self.L*math.cos(self._pitch)/2
+        height = self.L*math.sin(self._pitch)/2
         bPosition = [0, self._ycord+ycord, self._height+height]
         self._ycord += 2*ycord
         self._height += 2*height
